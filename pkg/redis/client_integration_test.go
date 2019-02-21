7af225ff960126c6985f53a03d67b37c772ad4a2
klimov sergey <ksa@introphin.com>
2019-02-21 19:06:13 +0300

    more integration tests

--- pkg/redis/client_integration_test.go
+++ pkg/redis/client_integration_test.go
@@ -3,6 +3,7 @@
 package redis
 
 import (
+	"github.com/go-redis/redis"
 	"github.com/klim0v/sequence-hashing/pkg/entity"
 	"testing"
 )
@@ -12,11 +13,28 @@ func TestClientPushPop(t *testing.T) {
 	client := NewClient()
 	_ = client.Push(&entity.Result{Number: number})
 	recvResult, _ := client.Pop()
-	_ = client.rc.Close()
 	if recvResult == nil {
 		t.Fatal("result is nil")
 	}
 	if recvResult.Number != number {
 		t.Error("not equal")
+	}
+}
+
+func TestClientPopUnmarshal(t *testing.T) {
+	client := NewClient()
+	client.rc.RPush(key, "test")
+	_, err := client.Pop()
+	if err != nil && err == redis.Nil {
+		t.Error(err)
+	}
+}
+
+func TestClientPopEmpty(t *testing.T) {
+	client := NewClient()
+	client.rc.Del(key)
+	_, err := client.Pop()
+	if err != nil && err != redis.Nil {
+		t.Error(err)
 	}
 }
